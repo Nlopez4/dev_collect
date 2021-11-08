fe280ff144d2a47bb48362b2516dd8794a8c08a7
nataliel2514 <natalielopez2514@gmail.com>
2021-11-08 16:17:54 -0600

    updated the function to a class

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # Post Project
     path('project/', views.ProjectList.as_view(),name="project_list"),
     path('project/<int:pk>/', views.ProjectDetail.as_view(), name="project_detail"),
-    path('project/new/', views.createProject, name="project_create"),
+    #path('project/new/', views.createProject, name="project_create"),
+    path('project/new/', views.ProjectCreate.as_view(  ), name="project_create"),
 ]
