cb382c62798cc42a8033e375177fca3849594986
nataliel2514 <natalielopez2514@gmail.com>
2021-11-09 18:34:55 -0600

    alter skills

--- main_app/models.py
+++ main_app/models.py
@@ -27,7 +27,7 @@ class Project(models.Model):
         return self.title
     
 class Skills(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=500)
    
     def __str__(self):
         return self.name
