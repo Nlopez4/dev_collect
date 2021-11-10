8c66f7872749cba3007087dedf9e71ba018ad719
nataliel2514 <natalielopez2514@gmail.com>
2021-11-09 18:41:04 -0600

    add string to profile model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=300)
@@ -10,9 +12,10 @@ class Profile(models.Model):
     bio = models.TextField(max_length=3000)
     github = models.CharField(max_length=2000, null=True, blank=True)
     skills = models.ManyToManyField('Skills', blank=True)
-    
+
     def __str__(self):
-        return self.user
+        return str(self.user)
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -25,9 +28,10 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 class Skills(models.Model):
     name = models.CharField(max_length=500)
-   
+
     def __str__(self):
         return self.name
