9f39c36db6a8ce5497e63b191b45d76b0266c0b7
nataliel2514 <natalielopez2514@gmail.com>
2021-11-13 19:31:20 -0600

    add dj databsse url

--- dev_project/settings.py
+++ dev_project/settings.py
@@ -138,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/project/'
 LOGOUT_REDIRECT_URL = '/project/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
