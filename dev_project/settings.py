b9f2cfcc66dbd88064884f1cde5cadb85849f766
nataliel2514 <natalielopez2514@gmail.com>
2021-11-14 22:20:51 -0600

    add whitenoise

--- dev_project/settings.py
+++ dev_project/settings.py
@@ -54,7 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'dev_project.urls'
 
